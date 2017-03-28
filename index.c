00a7f6a638a3110a6d6954c0feff6a56bd3f5626
andrewleonard1693 <AJ@Andrews-MBP.home>
2017-03-28 05:03:11 -0400

    recursion works

--- index.c
+++ index.c
@@ -10,13 +10,20 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-// typedef struct _Node
-// {
-// 	char *word;
-// 	struct* _Node left;
-// 	struct* _Node right;
-// 	struct* _Node head;
-// }Node;
+typedef struct _fNode
+{
+	char *fileName;
+	int numberOfOccurrences;
+}
+typedef struct _wNode
+{
+	char *word;
+	struct* _Node left;
+	struct* _Node right;
+	struct* _fNode head;
+}wordNode;
+
+
 
 /*indexDirectory(directoryName, myIndex){
 	open directoryName
@@ -32,45 +39,60 @@
 	}
 }*/
 
-int recursive(DIR * d, struct dirent * s);
-int recursive(DIR * d, struct dirent * s)
+int is_dot_or_dot_dot(char const* name);
+int is_dot_or_dot_dot(char const* name)
 {
-	do
-	{
-		if(s!=NULL)
-		{
-			if(s->d_type==DT_REG) // regular file
-			{
-				printf("%s\n", "found a file!");
-				printf("%s\n",s->d_name);
-				return 0;
-			}	
-			else if(s->d_type==DT_DIR)	//	directory
-			{
-				printf("%s\n", "found a directory!");
-				printf("%s\n",s->d_name);
-				s=readdir(d);
-				// d=opendir(s->d_name);
-				recursive(d,s);
-				closedir(d);
-				return 0;
-			}
-			else
-			{
-				printf("%s\n", "Found something that is not a directory or a file. Exiting indexer.");
-			}
-
-
-			// nameLen = strlen(d_name);
-			// result = write(fd, something->d_name, nameLen);
-		}
-	}while(s!=NULL);
-	
-	closedir(d); //closing the directory
-	return 0;
+   return (strcmp(name, ".") == 0 || strcmp(name, "..") == 0 );
 }
+
+void listdir(char const* dirname);
+void listdir(char const* dirname)
+{
+   char* subdir;
+   DIR* dirp = opendir(dirname);
+   struct dirent *curr_ent;
+
+   if ( dirp == NULL )
+   {
+      return;
+   }
+
+   while ( (curr_ent = readdir(dirp)) != NULL )
+   { 
+   	if(curr_ent->d_type==DT_REG)
+   	{
+   		printf("%s\n", curr_ent->d_name);	
+   	}
+
+      // Traverse sub-directories excluding . and ..
+       // Ignore . and ..
+      if (curr_ent->d_type == DT_DIR && !(is_dot_or_dot_dot(curr_ent->d_name)) )
+      {
+         // Allocate memory for the subdirectory.
+         // 1 additional for the '/' and the second additional for '\0'.
+         subdir = malloc(strlen(dirname) + strlen(curr_ent->d_name) + 2);
+
+         // Flesh out the subdirectory name.
+         strcpy(subdir, dirname);
+         strcat(subdir, "/");
+         strcat(subdir, curr_ent->d_name);
+
+         // List the contents of the subdirectory.
+         listdir(subdir);
+
+         // Free the allocated memory.
+         free(subdir);
+      }
+   }
+
+   // Close the directory
+   closedir(dirp);
+}
+
 int main(int argc, char const *argv[])
 {
+	struct *wordNode root =NULL;
+
 	struct dirent *something = NULL;
 	int status;
     struct stat st_buf;
@@ -87,14 +109,7 @@ int main(int argc, char const *argv[])
     }
     if (S_ISDIR (st_buf.st_mode)) {
         printf ("%s is a directory.\n", argv[1]);
-        DIR *directory =opendir(argv[1]); //opening a directory in current working directory
-			if(directory==NULL)
-			{
-				printf("%s\n", "Argument is not a directory. Exiting indexer.");
-
-			}
-		something = readdir(directory);
-		recursive(directory, something);
+		listdir(argv[1]);
 		return 0;
     }
 
